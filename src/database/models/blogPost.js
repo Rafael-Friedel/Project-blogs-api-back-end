6db5e8a389925c3ce93da098bb4beddcb504a0c1
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-07-15 13:36:57 -0300

    feat:requisito 10, models BlogPost, association com user

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,51 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    "BlogPost",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      published: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    { timestamps: false }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: "userId",
+      as: "user",
+    });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
